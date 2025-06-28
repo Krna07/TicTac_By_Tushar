05409cacea03afe4e729857ef24b9cc53d07f614
Krna07 <Krna260824@gmail.com>
2025-06-28 22:39:09 +0530

    Add Netlify _redirects file for React Router

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: '/', // should be this unless deploying to a subfolder
 })
